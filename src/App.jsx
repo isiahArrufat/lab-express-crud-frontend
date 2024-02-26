6de5bd5b2fc4ae0234c6520a0ededf81c6e14a26
Isiah Arrufat <isiaharrufat@pursuit.org>
2024-02-26 00:02:19 -0500

    commpeted frontend and abck end of part one

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { useState } from "react";
+import Logs from "./Logs";
+
 
 const App = () => {
+  const [allLogs, setAllLogs] = useState([])
+  
+
   return (
     <div>
       <h1>Logs CRUD</h1>
+     <Logs 
+     logs={allLogs} 
+     setAllLogs={setAllLogs}
+     />
     </div>
   );
 };
---
+++ src/Logs.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+
+
+const Logs = ({ logs, setAllLogs }) => {
+        useEffect(() => {
+            fetch("http://localhost:3003/api/logs")
+            .then((res) => res.json())
+            .then((data) => setAllLogs(data.logs))
+        }, [])
+
+        if(logs?.length === 0) return null;
+
+        return (
+            <div>
+                <h1>Logs</h1>
+                {logs.map(({ id, captainName, title, post, mistakesWereMandToday, daysSinceLastCrisis }) => 
+                    <div key={id}>
+                      <h3>Captain Name: {captainName}</h3>
+                      <h3>Title: {title}</h3>
+                      <p>Post: {post}</p>
+                      <h3>Mistakes Were Made Today?: {mistakesWereMandToday === true ? "Yes" : "No"}</h3>
+                      <h3>Days Since Last Crisis: {daysSinceLastCrisis}</h3>
+                      <hr />
+                    </div>
+                )}
+            </div>
+        )
+
+    }
+
+export default Logs
